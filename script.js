a5b1bc267e80af0293cdcf9c70137a8d9cd1694e
yutaasakura96 <yuta.asakura.se@gmail.com>
2024-01-28 18:44:58 +0900

    Implementing game logic
    -Using DOM manipulation and if/else statements to implement game logic within the application.

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 'use strict';
 
+// SECRET NUMBER GENERATOR
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+// DEFAULT APPLICATION STATE VARIABLES
+let score = 20;
+let highScore = 0;
+
+// CHECK BUTTON FUNCTIONALITY
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
+
+  // GAME LOGIC
+  //-GUESSING THE NUMBER
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
