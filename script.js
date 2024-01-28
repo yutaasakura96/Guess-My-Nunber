58f32a55b2e8af92158bc6cca60eae908764d1a5
yutaasakura96 <yuta.asakura.se@gmail.com>
2024-01-28 20:45:21 +0900

    Manipulating CSS styles
    -Using DOM manipulation to adjust CSS styles dynamically
    -Integrating functionality for the 'again' button, resetting the application to its default state.

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 'use strict';
 
-// SECRET NUMBER GENERATOR
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
-// DEFAULT APPLICATION STATE VARIABLES
+// DEFAULT APPLICATION STATE
 let score = 20;
 let highScore = 0;
+
+// SECRET NUMBER GENERATOR
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
 
 // CHECK BUTTON FUNCTIONALITY
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(typeof guess, guess);
 
   // GAME LOGIC
   //-GUESSING THE NUMBER
   if (!guess) {
+    // NO INPUT NUMBER
     document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // PLAYER WINS
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // WRONG GUESS: TOO HIGH
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -28,6 +36,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //  WRONG GUESS: TOO LOW
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
@@ -38,4 +48,19 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// AGAIN BUTTON FUNCTIONALITY
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  highScore = 0;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
